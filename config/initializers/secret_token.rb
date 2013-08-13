ba4476b24952a30c1f44b1ef2b2c2c41d22688f7
arhaine <arhaine@gmail.com>
2013-08-13 21:44:27 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyGarageTaskManager::Application.config.secret_key_base = '99940cacbca1a8bcc4e0954c499f94d7fc0c7732546c8ffcdee8c9f642749d01db05f7f80bdc5afa1ef12bf4708fc593394668ea4f98decaeb397500f911d5d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyGarageTaskManager::Application.config.session_store :cookie_store, key: '_RubyGarageTaskManager_session'
