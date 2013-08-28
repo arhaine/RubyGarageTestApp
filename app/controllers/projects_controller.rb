77e56b543beb7f5ceaae041e50ccc4b87a079c8f
Alex Rhaine <arhaine@localhost.localdomain>
2013-08-29 00:53:54 +0300

    Add Project and partially Task forms

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,14 +3,26 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    proj = Project.create name: params[:name], user: User.first
+    if proj.valid?
+      redirect_to '/'
+    end
   end
 
   def edit
+    @project = Project.find params[:id]
   end
 
   def update
+    @project = Project.find params[:id]
+    if @project.update_attributes(name: params[:project][:name])
+      flash[:success] = 'Update succeeded'
+      redirect_to '/'
+    end
   end
 
   def destroy
+    Project.destroy params[:id]
+    redirect_to '/'
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,11 @@
 class TasksController < ApplicationController
   def create
+    task = Task.create name: params[:name], project_id: params[:project_id], is_completed: false
+    if task.valid?
+      redirect_to '/'
+    else
+      pp task.errors
+    end
   end
 
   def edit
