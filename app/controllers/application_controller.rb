999bde8c390a14e70dec77e082fde4f73cb4e866
arhaine <arhaine@gmail.com>
2013-08-30 00:31:14 +0300

    Users signup, signin, signout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  include SessionsHelper
   def index
-    @projects_all = Project.all
+    if current_user
+      @projects_all = Project.where user_id: current_user.id
+    else
+      render :not_signed_in, layout: false
+    end
   end
 end
